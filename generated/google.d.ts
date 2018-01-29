4006fbb9a5c2d341913884d9ab6b8f97228868b5
John Johnson <johnrjj@gmail.com>
2018-01-29 18:01:49 -0500

    meh

---
+++ generated/google.d.ts
@@ -0,0 +1,5 @@
+// package: 
+// file: google
+
+import * as jspb from "google-protobuf";
+
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,35 @@
 import * as grpc from 'grpc';
 import * as fs from 'fs';
-import { GetInfoResponse } from './generated/rpc_pb';
+import * as path from 'path';
 
-
-// grpc = require('grpc');
-// var fs = require("fs");
 
 //  Lnd cert is at ~/.lnd/tls.cert on Linux and
 //  ~/Library/Application Support/Lnd/tls.cert on Mac
 import * as caller from 'grpc-caller';
 // const caller = require('grpc-caller')
 
-import * as services from './generated/rpc_pb';
+
+
+
 
 const lndCert = fs.readFileSync("/root/.lnd/tls.cert");
 const credentials = grpc.credentials.createSsl(lndCert);
-// const lnrpcDescriptor = grpc.load("rpc.proto");
-// const lnrpc = lnrpcDescriptor.lnrpc;
-// const lightning = new lnrpc.Lightning('localhost:10009', credentials);
+const lnrpcDescriptor = grpc.load("protos/rpc.proto");
+const lnrpc = lnrpcDescriptor.lnrpc;
+console.log(lnrpcDescriptor);
+console.log(lnrpcDescriptor.lnrpc);
+const lightning = new (lnrpc as any).Lightning('localhost:10009', credentials);
 
-const client = caller('localhost:10009', services, 'lnrpc', credentials);
-client.getInfo({}).then((x: any) => console.log(x)).catch((e: any) => console.log(e));
+const PROTO_PATH = path.resolve(__dirname, '../protos/rpc.proto')
+const client = caller('localhost:10009', PROTO_PATH, 'lnrpc', credentials)
+client.getInfo({ name: 'Bob' }, (err: any, res: any) => {
+  console.log(res)
+  console.log(err)
+  console.log('reeee');
+})
+
+// const client = caller('localhost:10009', services, 'lnrpc', credentials);
+// client.getInfo({}).then((x: any) => console.log(x)).catch((e: any) => console.log(e));
 
 // lightning.getInfo({}, (err: any, response: GetInfoResponse.) => {
 //   response.toObject().
