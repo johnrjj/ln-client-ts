1c8c8148d2df69e3b5e6fec721b3ee8f320f2eed
John Johnson <johnrjj@gmail.com>
2018-01-30 18:14:34 -0500

    trying to fix sendpayment

--- src/LightningRepository.ts
+++ src/LightningRepository.ts
@@ -3,29 +3,52 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as byteBuffer from 'bytebuffer';
 import * as caller from 'grpc-caller';
-import { GetInfoResponse, GraphTopologyUpdate } from './generated/rpc_pb';
 import { Duplex } from 'stream';
+
+export interface GetInfoReponse {
+  alias: string;
+  num_pending_channels: number;
+  num_active_channels: number;
+  num_peers: number;
+  block_height: number;
+  block_hash: string;
+  synced_to_chain: boolean;
+  testnet: boolean;
+  chains: Array<string>;
+  uris: Array<string>;
+};
 
 export class LNRepositoryPlexer extends Duplex {
   client: any;
   constructor() {
     super({ objectMode: true, highWaterMark: 1024 });
 
+    // less hardcoded plz
     const lndCert = fs.readFileSync('/root/.lnd/tls.cert');
-    // const lndCert = fs.readFileSync('../tls.cert');
     const credentials = grpc.credentials.createSsl(lndCert);
     const PROTO_PATH = path.resolve(__dirname, '../protos/rpc.proto');
     const client = caller('localhost:10009', PROTO_PATH, 'Lightning', credentials);
     this.client = client;
   }
 
-  async getInfo(): Promise<any> {
+  // works
+  async getInfo(): Promise<GetInfoReponse> {
     return this.client.getInfo({});
   }
 
   async sendPayment() {
-    const { call, res } = this.client.sendPayment({});
-    console.log(call, res);
+
+    var dest_pubkey = 'ss';
+    var dest_pubkey_bytes = byteBuffer.fromHex(dest_pubkey);
+    const { call, res, ...rest } = this.client.sendPayment({});
+
+    // call.write({ dest: dest_pubkey_bytes, amt: 6969 });
+    const sampleInvoice = 'lntb10n1pd8plq3pp5ernvlhrxtdcj9uxe8f3sx20zgk6l99fy4q0leech5umr0lv4m0yqdz80v3xjg36ygekzdn9x4jkxcfdv3jrqwfdxsukgc3d8ymrscedv43xzenpxqerxvn98yejylgcqzysu8w4fj9mklm7g460ncu9xpql6evvzr8hc8uxyr3sy8c2pyvgecfzy5gwr99s47q5yk2maqz8hn2z325urmgdn3kqqhhnrtruertfm0gph7aw40';
+    const x = call.write({ payment_request: sampleInvoice });
+    console.log(x);
+
+    console.log('inside sendpayment, call and res: ', call, res);
+    console.log('rest of obj', rest);
 
     // figure out how all this works together....
     // call.on('data', (msg: any) => {
